276b49c49877e46c15bc108a012bb38e8e4803ba
Gianluca Tallè <gianlucatalle96@gmail.com>
2025-08-06 19:19:55 +0200

    fix

--- app/Http/Controllers/CartItemsController.php
+++ app/Http/Controllers/CartItemsController.php
@@ -3,11 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\CartItem;
-use App\Models\Event;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class CartItemsController extends Controller
